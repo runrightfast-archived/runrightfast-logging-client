2cb10c8509735398ca8f8394a0a98d201cdad22d
Alfio Zappala <azappala@azaptree.com>
2013-09-19 03:51:21 -0400

    100% code coverage

--- test/logging-client.js
+++ test/logging-client.js
@@ -46,7 +46,7 @@ describe('LoggingClient', function() {
 
 	after(function(done) {
 		composer.stop({
-			timeout : 1 * 1000
+			timeout : 1000
 		}, function() {
 			console.log('All servers stopped');
 			done();
@@ -65,6 +65,59 @@ describe('LoggingClient', function() {
 		loggingClient.log(event);
 		expect(loggingClient.eventCount).to.equal(1);
 		expect(loggingClient.invalidEventCount).to.equal(0);
+	});
+
+	it('log a valid event with timeout of 100 msec', function() {
+		var loggingClient = require('..')({
+			url : 'http://localhost:8000/log',
+			timeout : 100
+		});
+		var event = {
+			tags : [ 'info' ],
+			data : 'test : log a valid event with timeout of 100 msec'
+		};
+
+		for ( var i = 0; i < 10; i++) {
+			loggingClient.log(event);
+		}
+		expect(loggingClient.eventCount).to.equal(10);
+		expect(loggingClient.invalidEventCount).to.equal(0);
+	});
+
+	it('log a valid event to an invalid URL', function(done) {
+		var loggingClient = require('..')({
+			url : 'http://localhost:8000/logXQZ',
+			timeout : 10,
+			retry : {
+				initial : 1,
+				multiplier : 2,
+				max : 2
+			},
+			errorCallback : function() {
+				done();
+			}
+		});
+		var event = {
+			tags : [ 'info' ],
+			data : 'test : log a valid event to an invalid URL'
+		};
+
+		loggingClient.log(event);		
+		expect(loggingClient.eventCount).to.equal(1);
+		expect(loggingClient.invalidEventCount).to.equal(0);
+	});
+
+	it('log an invalid event', function() {
+		var loggingClient = require('..')({
+			url : 'http://localhost:8000/log'
+		});
+		var event = {
+			data : 'test : log an invalid event'
+		};
+
+		loggingClient.log(event);
+		expect(loggingClient.eventCount).to.equal(0);
+		expect(loggingClient.invalidEventCount).to.equal(1);
 	});
 
 });
