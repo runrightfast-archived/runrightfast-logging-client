c9c0856f4f256e20cd1e4b1425df05cd478bd060
Alfio Zappala <azappala@azaptree.com>
2013-09-19 02:50:13 -0400

    initial implementation of REST client with basic testing

--- test/logging-client.js
+++ test/logging-client.js
@@ -1,2 +1,70 @@
 'use strict';
 var expect = require('chai').expect;
+
+function startServer(callback) {
+	var Hapi = require('hapi');
+
+	var manifest = {
+		pack : {},
+		servers : [ {
+			port : 8000,
+			options : {
+				labels : [ 'web' ]
+			}
+		} ],
+		plugins : {
+			'lout' : {},
+			'furball' : {},
+			'runrightfast-logging-service-hapi-plugin' : {}
+		}
+	};
+
+	var composer = new Hapi.Composer(manifest);
+
+	composer.compose(function(err) {
+		if (err) {
+			console.error('Failed composing servers : ' + err.message);
+			callback(err);
+		} else {
+			console.log('Hapi is composed.');
+			composer.start(function() {
+				console.log('All servers started');
+				callback();
+			});
+		}
+	});
+
+	return composer;
+}
+
+describe('LoggingClient', function() {
+	var composer = null;
+
+	before(function(done) {
+		composer = startServer(done);
+	});
+
+	after(function(done) {
+		composer.stop({
+			timeout : 1 * 1000
+		}, function() {
+			console.log('All servers stopped');
+			done();
+		});
+	});
+
+	it('log a valid event', function() {
+		var loggingClient = require('..')({
+			url : 'http://localhost:8000/log'
+		});
+		var event = {
+			tags : [ 'info' ],
+			data : 'test : log a valid event'
+		};
+
+		loggingClient.log(event);
+		expect(loggingClient.eventCount).to.equal(1);
+		expect(loggingClient.invalidEventCount).to.equal(0);
+	});
+
+});
